642e0d684e0a0872ff1163da6a02685a39a0d269
Osvaldas Bernatavičius <Osvaldas.Bernatavicius@4finance.com>
2022-05-25 21:58:48 +0300

    refactored

--- expenses-calculator/src/componets/Expenses/ExpenseItem.js
+++ expenses-calculator/src/componets/Expenses/ExpenseItem.js
@@ -1,25 +1,15 @@
-import { useState } from "react";
-
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    console.log("test");
-    setTitle("test");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>ping</button>
     </Card>
   );
 };
